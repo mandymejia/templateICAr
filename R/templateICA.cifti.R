f0e345556b83368d706e8cf5437faa309294d854
Damon Pham <damondpham@gmail.com>
2021-01-15 09:51:01 -0800

    try to bring back brainstructures check

--- R/templateICA.cifti.R
+++ R/templateICA.cifti.R
@@ -68,10 +68,10 @@ templateICA.cifti <- function(cifti_fname,
   if (is.null(write_dir)) { write_dir <- getwd() }
 
   # TEMPORARY 
-  # brainstructures <- match_input(
-  #   brainstructures, c("left","right","subcortical","all"),
-  #   user_value_label="brainstructures"
-  # )
+  brainstructures <- match_input(
+    brainstructures, c("left","right","subcortical","all"),
+    user_value_label="brainstructures"
+  )
   if ("all" %in% brainstructures) {
     brainstructures <- c("left","right","subcortical")
   }
