c150ef5b6c9d2f47b66be773723e3af3d1c73391
Dan <danieladamspencer@users.noreply.github.com>
2022-07-05 15:42:52 -0400

    Cpp Gibbs function is tested and matching R

--- src/EM_FCtempICA.cpp
+++ src/EM_FCtempICA.cpp
@@ -5,6 +5,24 @@
 using namespace Rcpp;
 using namespace Eigen;
 
+//' Update parameters (M-step of the EM)
+//'
+//' @param template_mean a matrix with dimensions V x Q giving the mean value
+//'   of the independent components
+//' @param template_var a matrix with dimensions V x Q giving the variance
+//'   of the independent components
+//' @param template_FC a list with two elements: psi, a Q x Q matrix, and nu, a
+//'   scalar. These two values are the parameters of the Wishart prior on G
+//' @param G a Q x Q matrix of the prior covariance of A
+//' @param prior_params a length 2 vector with the prior parameters for tau_v
+//' @param BOLD a V x T matrix of BOLD values
+//' @param Y_sq_sum a length V vector with the sum of squared BOLD values at
+//'   each data location
+//' @param post_sums a list of posterior summary statistics including the named
+//'   summaries \code{AS_sq_sum}, \code{yAS_sum}, \code{A_sum}, and \code{AtA_sum}
+//' @param sigma2_alpha a scalar multiplier for the prior variance of alpha
+//' @param verbose a boolean. Should messages be generated and output?
+//' @export
 // [[Rcpp::export]]
 Rcpp::List UpdateTheta_FCtemplateICAcpp(Eigen::MatrixXd template_mean,
                                         Eigen::MatrixXd template_var,
@@ -44,6 +62,10 @@ Rcpp::List UpdateTheta_FCtemplateICAcpp(Eigen::MatrixXd template_mean,
     tau_sq_new(v) = tau_sq_num / tau_sq_den;
     // tau_sq_new(v) = 1/(ntime + 2*alpha_tau + 2) * (AS_sq(v) - 2*yAS(v) + 2*beta_tau);
   }
+  double tau_mean = tau_sq_new.mean();
+  for(int v=0;v<nvox;v++){
+    tau_sq_new(v) = tau_mean;
+  }
   // UPDATE ALPHA (TEMPORAL INTERCEPT)
   Eigen::MatrixXd alpha_part = Eigen::MatrixXd::Identity(nICs,nICs);
   Eigen::MatrixXd G_inv = G.inverse();
@@ -65,6 +87,22 @@ Rcpp::List UpdateTheta_FCtemplateICAcpp(Eigen::MatrixXd template_mean,
                             Rcpp::Named("G") = G_newX);
 }
 
+//' Use a Gibbs sampler for the A and S variables (E-step of the EM)
+//'
+//' @param nsamp the number of posterior samples to output after burn-in
+//' @param nburn the number of posterior samples to throw away before saving
+//' @param template_mean a matrix with dimensions V x Q giving the mean value
+//'   of the independent components
+//' @param template_var a matrix with dimensions V x Q giving the variance
+//'   of the independent components
+//' @param S a matrix with dimensions V x Q of subject independent components
+//' @param G a Q x Q matrix of the prior covariance of A
+//' @param tau_v a length V vector with noise variance for each data location
+//' @param Y a matrix with dimensions V x T of observed BOLD data
+//' @param alpha a length Q vector of the prior mean of all rows of A
+//' @param final a boolean. Should posterior samples be returned instead of
+//'   summary measures?
+//' @export
 // [[Rcpp::export]]
 Rcpp::List Gibbs_AS_posteriorCPP(const int nsamp, const int nburn,
                                  const Eigen::MatrixXd template_mean,
@@ -101,7 +139,10 @@ Rcpp::List Gibbs_AS_posteriorCPP(const int nsamp, const int nburn,
     AS_sq = Eigen::VectorXd::Zero(V);
   }
   // Calculate static quantities
-  Eigen::VectorXd tau_inv = tau_v.cwiseInverse();
+  double mean_tau = tau_v.mean();
+  Eigen::VectorXd tau_inv(V);
+  for(int v=0;v<V;v++) tau_inv(v) = 1 / mean_tau;
+  // Eigen::VectorXd tau_inv = tau_v.cwiseInverse();
   Eigen::MatrixXd G_tau_inv = tau_inv.asDiagonal();
   Eigen::MatrixXd G_inv = G.inverse();
   Eigen::VectorXd alphaGinv = G_inv * alpha;
@@ -125,7 +166,6 @@ Rcpp::List Gibbs_AS_posteriorCPP(const int nsamp, const int nburn,
   // Start the Gibbs sampler
   for(int i=1;i <= niter; i++) {
     // Update A
-    // Rcout << "Update A" << std::endl;
     SGti = S.transpose() * G_tau_inv;
     sig_inv_A = SGti * S;
     sig_inv_A += G_inv;
@@ -142,18 +182,16 @@ Rcpp::List Gibbs_AS_posteriorCPP(const int nsamp, const int nburn,
       A.row(t) = At;
     }
     // Update S
-    // Rcout << "Update A" << std::endl;
     AtA = A.transpose() * A;
     for(int v=0; v < V; v++) {
       sig_inv_S = AtA;
       sig_inv_S /= tau_v(v);
-      // sig_inv_S += template_var.row(v).asDiagonal();
       sig_inv_S += G_sv_inv.row(v).asDiagonal();
       chol_sig_inv_S.compute(sig_inv_S);
       chol_sig_S = chol_sig_inv_S.matrixL().toDenseMatrix().inverse();
       AtYvtempVarMean = A.transpose() * Y.row(v).transpose();
       AtYvtempVarMean /= tau_v(v);
-      tVarMean = template_var.row(v).asDiagonal() * template_mean.row(v).transpose();
+      tVarMean = G_sv_inv.row(v).asDiagonal() * template_mean.row(v).transpose();
       AtYvtempVarMean += tVarMean;
       mu_sv = chol_sig_inv_S.solve(AtYvtempVarMean);
       Z = rnorm(Q);
