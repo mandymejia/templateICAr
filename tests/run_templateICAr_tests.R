ce92213b84fabd98fd6c47a7c516676f7d0dd0a7
Damon Pham <damondpham@gmail.com>
2023-04-05 06:06:20 +0700

    add back test scripts

---
+++ tests/run_templateICAr_tests.R
@@ -0,0 +1,18 @@
+# Build --> Install and Restart
+
+# [Edit these]
+## path to your Connectome Workbench
+my_wb <- "~/Desktop/workbench"
+## path to test data
+dir_data <- "tests/data"
+## path to results from tests
+dir_results <- file.path(dir_data, "results")
+
+library(testthat)
+library(ciftiTools)
+if (interactive()) { ciftiTools.setOption('wb_path', my_wb) }
+library(templateICAr)
+
+tests_dir <- "testthat"
+if (!endsWith(getwd(), "tests")) { tests_dir <- file.path("tests", tests_dir) }
+source(file.path(tests_dir, "test-misc.R"))
