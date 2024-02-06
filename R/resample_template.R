efe3855e3e48edc934bae1184dd445a0d36949f4
Damon Pham <damondpham@gmail.com>
2024-02-06 23:19:30 +0700

    rox and fix

--- R/resample_template.R
+++ R/resample_template.R
@@ -1,14 +1,14 @@
 #' Resample CIFTI template
-#' 
+#'
 #' Resample a CIFTI template to a new spatial resolution.
-#' 
+#'
 #' @param x The \code{"template.cifti"} object.
 #' @param resamp_res The new resampling resolution.
 #' @param verbose Give occasional updates? Default: \code{FALSE}.
-#' 
+#'
 #' @return The resampled \code{"template.cifti"} object.
 #' @export
-#' 
+#'
 resample_template <- function(x, resamp_res, verbose=FALSE){
   stopifnot(inherits(x, "template.cifti"))
 
@@ -18,7 +18,7 @@ resample_template <- function(x, resamp_res, verbose=FALSE){
 
   # Resample the data.
   if (verbose) { cat("Resampling templates.\n") }
-  x$template <- lapply(x$template, function(y){
+  x$template[names(x$template) != "FC"] <- lapply(x$template[names(x$template) != "FC"], function(y){
     as.matrix(ciftiTools::resample_xifti(ciftiTools::newdata_xifti(x$dat_struct, y), resamp_res=resamp_res, verbose=FALSE))
   })
   if (verbose) { cat("Resampling variance decomposition.\n") }
@@ -28,7 +28,7 @@ resample_template <- function(x, resamp_res, verbose=FALSE){
 
   if (verbose) { cat("Formatting new template object.\n") }
   # Replace `NaN` values with NA values.
-  x$template <- lapply(x$template, function(y){y[] <- ifelse(is.nan(y), NA, y)})
+  x$template[names(x$template) != "FC"] <- lapply(x$template[names(x$template) != "FC"], function(y){y[] <- ifelse(is.nan(y), NA, y)})
   x$var_decomp <- lapply(x$var_decomp, function(y){y[] <- ifelse(is.nan(y), NA, y)})
 
   # Get new `dat_struct` and mask.
